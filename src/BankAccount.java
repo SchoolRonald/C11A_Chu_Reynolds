e2c57ea9d5b5d9977e3837ebf279378fde84fdf3
Ronald Chu <rchu@mines.edu>
2025-10-03 14:40:52 -0600

    Added comments to current code for BankAccount and NegativeBalanceException

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -2,26 +2,40 @@
 
 
 public class BankAccount {
+	
+	//creates an instance variable of type double named balance
 	private double balance;
 	
+	
+	//1-parameter constructor that sets the balance
 	BankAccount (double balance) {
 		this.balance = balance;
 	}
 	
+	//instance method that takes a a parameter of double amount and throws negative balance
 	public void withdraw(double amount) throws NegativeBalanceException {
 		
+		//if the amount to withdraw is greater than the balance, throws
 		if (amount > this.balance) {
 			throw new NegativeBalanceException(this.balance - amount);
 		}
+		
+		//otherwise it will update the balance (subtracting the amount being withdrawn)
 		else {
 			this.balance =  this.balance - amount;
 		}
 	}
 	
+	
+	//instance method that also takes the amount from the user along with a throws NegativeBalanceException
 	public void quickWithdraw(double amount) throws NegativeBalanceException {
+		
+		//if the amount withdrawn exceeds the balance, it will throw a NegativeBalanceException
 		if (amount > this.balance) {
 			throw new NegativeBalanceException();
 		}
+		
+		//otherwise it will update the balance with the new balnace
 		else {
 			this.balance =  this.balance - amount;
 		}
--- src/NegativeBalanceException.java
+++ src/NegativeBalanceException.java
@@ -3,24 +3,47 @@ import java.io.*;
 
 
 public class NegativeBalanceException extends Exception {
+	
+	//one instance variable of type double named negativeBalance
 	private double negativeBalance;
 	
-	
+	//default constructor that calls super() and sets the exception message
+	//to Error: negative balance
 	NegativeBalanceException() {
 		super("Error: negative balance");
 	}
 	
+	
+	//A 1-parameter constructor that sets the instance variable and calls super()
+	// with the message "Amount exceeds balance by [negative balance]
 	NegativeBalanceException(double negative) {
 		super("Amount Exceeds balance by " + negative);
+		
+		//assigns the objects negative balance, to the negative balance
+		//that is inputed
 		this.negativeBalance = negative;
+		
+		//write the same message to a log file
+		PrintWriter out;
+		
+		//try and catch to make sure that logfile is printed out too
 		try {
-			FileWriter writer = new FileWriter("logfile.txt");
-			writer.write("Amount Exceeds balance by " + negative);
-		} catch (IOException e) {
-			e.printStackTrace();
+			//declares and prints out the amount exceeds balance by [negative balance]
+			out = new PrintWriter("logfile.txt");
+			out.println("Amount Exceeds balance by " + this.negativeBalance);
+			
+			//must close the writer to finalize file
+			out.close();
+			
+		} catch (FileNotFoundException e) {
+			
+			//prints out that it could not output to logfile.txt if it fails
+			System.out.println("Could not output to logfile.txt");
 		}
+	
 	}
 
+	//create a toString to print out the Balance of [negative balance] not allowed
 	@Override
 	public String toString() {
 		return "Balance of " + negativeBalance + "not allowed";
